0a62487833969cd617b4667a07c857b9e50bc043
Samuel Jessop Kaleopa <97277648+Bambyboi@users.noreply.github.com>
2022-02-11 18:14:30 +1300

    Add files via upload

---
+++ game/director.py
@@ -0,0 +1,99 @@
+from game.console import Console
+from game.jumper import Jumper
+from game.word import Word
+
+class Director:
+    """A code template for a person who directs the game. The responsibility of 
+    this class of objects is to control the sequence of play.
+    
+    Stereotype:
+        Controller
+    Attributes:
+        console (Console): An instance of the class of objects known as Console.
+        keep_playing (boolean): Whether or not the game can continue.
+        
+    """
+
+    def __init__(self):
+        """The class constructor.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+
+        self.console = Console()
+        self.word = Word() #generate the word here. correctWord is generated on Word init
+        self.jumper = Jumper(self.word.correctWord) #needs the correct word to generate length of underscore array
+        self.keep_playing = True
+
+        
+    def start_game(self):
+        """Starts the game loop to control the sequence of play.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+
+        while self.keep_playing:
+            self.get_inputs()
+            self.do_updates()
+            self.do_outputs()
+
+    def get_inputs(self):
+        """Gets the inputs from user at the beginning of each round
+        of play. Send update array to jumper
+        Args:
+            self (Director): An instance of Director.
+        """
+        message = ""
+        for element in self.jumper.displayArray:    # Line added
+            message = message + element + " "     # Line added
+        self.console.write(message) # Original line
+        message = self.jumper.picture()
+        self.console.write(message)
+        oneLetterResponse = False
+        while not oneLetterResponse:
+            self.guess = self.console.read("Guess a letter [a-z]: ")
+            if len(self.guess) != 1:
+                print("\nPlease enter one letter. No more, no less.\n")
+            else:
+                oneLetterResponse = True
+
+        
+    def do_updates(self):
+        """Updates the important game information for each round of play.
+        check the word (boolean value).
+        Args:
+            self (Director): An instance of Director.
+        """
+        self.positionsOfCorrect = self.word.checkLetter(self.guess)
+        self.jumper.updateArray(self.positionsOfCorrect, self.guess)
+        self.checkVictory = self.jumper.checkVictory()
+        self.checkDefeat = self.jumper.checkDefeat()
+        
+    def do_outputs(self):
+        """Outputs the important game information for each round of play. In 
+            checkVictory
+            checkDefeat
+        Args:
+            self (Director): An instance of Director.
+        """
+        if self.checkVictory == True: #Checks to see if you've won. Still needs lots of testing
+            message = "Congratulations, you won! The word was: "
+            self.console.write(message)
+            message = "\n" + self.word.correctWord
+            self.console.write(message)
+
+            self.keep_playing = False #If you've won, game ends. 
+
+        elif self.checkDefeat == True: #checks to see if you've lost, still needs lots of testing. 
+            message = self.jumper.picture()
+            self.console.write(message)
+
+            message ="\nSorry! Try again! Your word was: "
+            self.console.write(message)
+
+            message ="\n" + self.word.correctWord
+            self.console.write(message)
+
+            self.keep_playing = False #if you've lost, game ends
---
+++ game/word.py
@@ -0,0 +1,56 @@
+import random
+
+class Word():
+    """A code template for getting a word for the user to guess. The responsibility of this
+    class object is to retrieve a word from a large list and check to see if the letter that
+    is guessed by the user is in the word or not.
+    Stereotype:
+        Information Holder
+    Attributes:
+        wordList (list): The list of words from the txt given text file.
+        correctWordList (string): Selects a new random word from the wordList file for each game.
+        correctWord (string): The word that will be used throughout the game session.
+    """
+    def __init__(self): #constructs the correctWord Variable from given text list.
+        """Class constructor. Declares and initializes instance attributes.
+        Args:
+            self (word): An instance of Word.
+        """
+        self.f = open("wordlist.txt", "r")
+        wordList = []
+        self.correctWordList = ""
+        self.correctWord = ""
+        for line in self.f:  #Converts text file into array
+            stripped_line = line.strip()
+            line_list = stripped_line.split()
+            wordList.append(line_list)
+        self.f.close()
+
+        self.correctWordList = wordList[random.randint(0 , len(wordList))] #Selects a random word between 0 and the length of the list. 
+
+        self.correctWord = self.correctWord.join(self.correctWordList)
+
+    def checkLetter(self, guess):
+        """Checks to see if the letter entered by the user is in the word and places it in the correct placement.
+        Args:
+            self (Word): An instance of Word.
+        Returns:
+            list: Updates the positions of the correct letters guessed.
+        """
+        positionsOfCorrect = []
+        i = 0 #i is the current position being scanned. 
+
+        for char in self.correctWord:
+
+            if char == guess: 
+                positionsOfCorrect.append(i) #i begins at zero, iterates after checking and posting the value
+                i = i + 1
+            
+            else: 
+                i = i + 1 #After checking position, i becomes new position .
+
+        #Returns the array that tell the program the POSITION of what answers were correct.
+        #Use the update array function to do array[positionsOfCorrect[i]] = guess
+        #Where i is the position of the positionsOfCorrect Variable
+
+        return positionsOfCorrect # this method is called in director
